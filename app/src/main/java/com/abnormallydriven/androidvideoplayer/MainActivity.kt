1a05243b7c3b0abc8d841506cb5203126ab59d6d
Brett <bretttucker@gmail.com>
2017-10-25 20:19:16 -0600

    Add dependencies

--- app/src/main/java/com/abnormallydriven/androidvideoplayer/MainActivity.kt
+++ app/src/main/java/com/abnormallydriven/androidvideoplayer/MainActivity.kt
@@ -2,13 +2,13 @@ package com.abnormallydriven.androidvideoplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("test", BuildConfig.YOUTUBE_API_KEY)
+
+
     }
 }
